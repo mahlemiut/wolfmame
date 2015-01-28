b82d5df09bf2d12ccdbe4c6d037b11f5da883ff0
mahlemiut <bsr@xnet.co.nz>
2015-01-28 22:19:50 +1300

    sync to 0.158 and version bump

--- src/version.c
+++ src/version.c
@@ -13,4 +13,4 @@
 extern const char bare_build_version[];
 extern const char build_version[];
 const char bare_build_version[] = BARE_BUILD_VERSION;
-const char build_version[] = BARE_BUILD_VERSION "/W 4/1/15";
+const char build_version[] = BARE_BUILD_VERSION "/W 28/1/15";

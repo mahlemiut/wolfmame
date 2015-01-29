c9ed7bc2090b3efd329600072bfe6425dc245af5
mahlemiut <bsr@xnet.co.nz>
2015-01-29 14:38:22 +1300

    fix record.bat script

--- record.bat
+++ record.bat
@@ -4,7 +4,7 @@ if exist hi\%1.hi move hi\%1.hi hi\%1.bak
 if exist nvram\%1.nv move nvram\%1.nv nvram\%1.bak
 if exist diff\%1.dif move diff\%1.dif diff\%1.bak
 
-mame %1 -playback %2.inp %3 %4 %5 %6 %7 %8 %9 -nvram_directory NUL
+mame %1 -record %2.inp %3 %4 %5 %6 %7 %8 %9 -nvram_directory NUL
 
 if exist cfg\%1.bak move cfg\%1.bak cfg\%1.cfg
 if exist hi\%1.bak move hi\%1.bak hi\%1.hi

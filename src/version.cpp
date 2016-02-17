1a21217e03395b55fecf2059d466ce47e5b2bdeb
mahlemiut <bsr@xnet.co.nz>
2016-02-17 13:10:30 +1300

    correct version string

--- src/version.cpp
+++ src/version.cpp
@@ -13,4 +13,4 @@
 extern const char bare_build_version[];
 extern const char build_version[];
 const char bare_build_version[] = BARE_BUILD_VERSION;
-const char build_version[] = BARE_BUILD_VERSION " " __DATE__;
+const char build_version[] = BARE_BUILD_VERSION "W " __DATE__;

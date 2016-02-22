c9a606c491474548c6437a42539f0e1c4450b98c
mahlemiut <bsr@xnet.co.nz>
2016-02-22 14:13:18 +1300

    added NVRAM, DIFF and Lua warnings, and make it start recording.

--- src/emu/ui/recinp.h
+++ src/emu/ui/recinp.h
@@ -13,6 +13,8 @@
 #ifndef __UI_RECINP_H__
 #define __UI_RECINP_H__
 
+#define TOTAL_WARNINGS 3
+
 class ui_menu_record_inp : public ui_menu
 {
 public:
@@ -23,8 +25,18 @@ public:
 	virtual void custom_render(void *selectedref, float top, float bottom, float x, float y, float x2, float y2) override;
 
 private:
+	void start_rec();
 	const game_driver *m_driver;
 	char m_filename_entry[100];
+
+	int m_warning_count;
+	bool m_warning[TOTAL_WARNINGS];
+	std::string m_warning_text[TOTAL_WARNINGS] =
+	{
+		"NVRAM folder for this system exists, and nvram_directory is not set to NUL or /dev/null.  NVRAM may be present, making playback for others difficult.  MARP requires INPs to be recorded with no NVRAM present.",
+		"CHD DIFF file for this system exists.  Playback may not be possible.",
+		"Lua console is active.  This can affect playbackability if used, and can also be used for cheating."
+	};
 };
 
 #endif /* __UI_RECINP_H__ */

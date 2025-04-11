fcdcfc3f07a67d4930c8f2066a334e76deb59d0e
mahlemiut <mahlemiut75@gmail.com>
2025-04-11 22:39:27 +1200

    ui.tcl: finally update to use MAME's -listxml output to get driver list

--- ui.tcl
+++ ui.tcl
@@ -20,125 +20,133 @@ button .browse -text "Browse..."
 
 # event procedures
 proc click_browse {} {
-set ftypes {
-	{{MAME input logs} {*.inp}}
-	{{All files} {*}}
-}
-set filename [tk_getOpenFile -filetypes $ftypes]
-.inppath delete 0 end ;# clear entry box
-.inppath insert 0 $filename
+    set ftypes {
+        {{MAME input logs} {*.inp}}
+        {{All files} {*}}
+    }
+    set filename [tk_getOpenFile -filetypes $ftypes]
+    .inppath delete 0 end ;# clear entry box
+    .inppath insert 0 $filename
 }
 
 proc click_record {} {
-global cmdfilename
-global gamelistshortnames
-global nullfile
-set sel [.gamelist curselection]
-if { $sel == "" } {
-	tk_messageBox -message "Please select a game from the list"
-	return
-}
-if { [.inppath get] == "" } {
-	tk_messageBox -message "Please enter an INP filename to record to"
-	return
-}
-if { [string match "*.inp" [.inppath get]] == 0} {
+    global cmdfilename
+    global gamelistshortnames
+    global nullfile
+    set sel [.gamelist curselection]
+    if { $sel == "" } {
+        tk_messageBox -message "Please select a game from the list"
+        return
+    }
+    if { [.inppath get] == "" } {
+        tk_messageBox -message "Please enter an INP filename to record to"
+        return
+    }
+    if { [string match "*.inp" [.inppath get]] == 0} {
         tk_messageBox -message "INP filenames require the .inp extension"
         return
-}
-set index [lindex $sel 0]
-set command $cmdfilename
-append command " "
-append command [lindex $gamelistshortnames $index]
-append command " -record "
-append command [.inppath get]
-append command " -nvram_directory "
-append command $nullfile
-append command " "
-append command [.param get]
-tk_messageBox -message $command
-set reccmd [open $command r]
-set output [read $reccmd]
-close $reccmd
-tk_messageBox -title "Console output" -message $output
+    }
+    set index [lindex $sel 0]
+    set command $cmdfilename
+    append command " "
+    append command [lindex $gamelistshortnames $index]
+    append command " -record "
+    append command [.inppath get]
+    append command " -nvram_directory "
+    append command $nullfile
+    append command " "
+    append command [.param get]
+    tk_messageBox -message $command
+    set reccmd [open $command r]
+    set output [read $reccmd]
+    close $reccmd
+    tk_messageBox -title "Console output" -message $output
 }
 
 proc click_playback {} {
-global cmdfilename
-global gamelistshortnames
-global nullfile
-set sel [.gamelist curselection]
-if { $sel == "" } {
+    global cmdfilename
+    global gamelistshortnames
+    global nullfile
+    set sel [.gamelist curselection]
+    if { $sel == "" } {
         tk_messageBox -message "Please select a game from the list"
         return
-}
-if { [.inppath get] == "" } {
+    }
+    if { [.inppath get] == "" } {
         tk_messageBox -message "Please enter an INP filename to playback"
         return
-}
-if { [string match "*.inp" [.inppath get]] == 0} {
-	tk_messageBox -message "INP filenames require the .inp extension"
-	return
-}
-set index [lindex $sel 0]
-set command $cmdfilename
-append command " "
-append command [lindex $gamelistshortnames $index]
-append command " -playback "
-append command [.inppath get]
-append command " -nvram_directory "
-append command $nullfile
-append command " "
-append command [.param get]
-tk_messageBox -message $command
-set playcmd [open $command r]
-set output [read $playcmd]
-close $playcmd
-tk_messageBox -title "Console output" -message $output
+    }
+    if { [string match "*.inp" [.inppath get]] == 0} {
+        tk_messageBox -message "INP filenames require the .inp extension"
+        return
+    }
+    set index [lindex $sel 0]
+    set command $cmdfilename
+    append command " "
+    append command [lindex $gamelistshortnames $index]
+    append command " -playback "
+    append command [.inppath get]
+    append command " -nvram_directory "
+    append command $nullfile
+    append command " "
+    append command [.param get]
+    tk_messageBox -message $command
+    set playcmd [open $command r]
+    set output [read $playcmd]
+    close $playcmd
+    tk_messageBox -title "Console output" -message $output
 }
 
 # figure out if we're using a 32-bit or 64-bit version of MAME
 set cmdfilename "|./mame"
 
 if { [catch {exec ./mame -help}] } {
-	set cmdfilename "|./mame64"
-	if { [catch {exec ./mame64 -help}] } {
-		set cmdfilename "none"
-		tk_messageBox -title "Error" -type ok -message "Cannot find a MAME executable."
-		exit 1
-	}
+    set cmdfilename "|./mame64"
+    if { [catch {exec ./mame64 -help}] } {
+        set cmdfilename "none"
+        tk_messageBox -title "Error" -type ok -message "Cannot find a MAME executable."
+        exit 1
+    }
 }
 
 # figure out the platform we are running on
 global tcl_platform
 switch -glob -- [lindex $tcl_platform(os) 0] {
-	Win* {
-		set nullfile "NUL"  ;# Windows
-	}
-	default {
-		set nullfile "/dev/null"  ;# everything else (except maybe old Macs)
-	}
+    Win* {
+        set nullfile "NUL"  ;# Windows
+    }
+    default {
+        set nullfile "/dev/null"  ;# everything else (except maybe old Macs)
+    }
 }
 
 # populate listbox
 set gamelistitems [list]
 set gamelistshortnames [list]
+
+# Parse MAME's -listxml output
+package require tdom
 set cmdline $cmdfilename
-append cmdline " -listfull"
+append cmdline " -listxml"
 set cmd [open $cmdline r]
-# TODO: handle ini parsing errors, which will screw this up
-gets $cmd ;# ignore first line, it contains the headers
-while {[gets $cmd line] >= 0} {
-	set gamedata [split $line "\t\""]
-	set str [string trim [lindex $gamedata 0] " "]
-	lappend gamelistshortnames $str
-	lappend gamelistitems [lindex $gamedata 1]
-	set listtext [lindex $gamedata 0]
-	append listtext [lindex $gamedata 1]
-	.gamelist insert end $listtext
-}
+set xml_data [read $cmd]
 close $cmd
 
+# Parse XML
+set doc [dom parse $xml_data]
+
+# Select all runnable machines
+set machines [$doc selectNodes {//machine[@runnable='yes']}]
+
+# Populate the listbox
+foreach machine $machines {
+    set name [$machine getAttribute name]
+    set description [$machine selectNodes description]
+    set description_text [$description asText]
+    lappend gamelistshortnames $name
+    lappend gamelistitems $description_text
+    .gamelist insert end "$name - $description_text"
+}
 
 # GUI management
 grid columnconfigure . {0 1 2 3 4} -weight 1
